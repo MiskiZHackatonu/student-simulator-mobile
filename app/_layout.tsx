6a857a5c2b211f8863f9f2381b5ce21cf37040e5
Patryk Klatka <patryk.klatka@gmail.com>
2024-05-18 10:57:35 +0200

    feat: check camera permissions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,11 +4,10 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { router, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
 
@@ -22,22 +21,9 @@ export default function RootLayout() {
   });
 
   useEffect(() => {
-    const load = async () => {
-      if (loaded) {
-        // If nick is not set, we have to show onboarding screen
-        const nick = await AsyncStorage.getItem("nick");
-
-        if (true) {
-          router.replace("/games");
-        } else {
-          router.replace("/onboarding");
-        }
-
-        SplashScreen.hideAsync();
-      }
-    };
-
-    load();
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
   }, [loaded]);
 
   if (!loaded) {
