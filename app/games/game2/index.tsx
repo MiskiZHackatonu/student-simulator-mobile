c11eb4ac3c7d7d7fddee1563cff49c6ad3f012da
MyKarcio123 <karol.wieckowiak21@interia.pl>
2024-05-19 09:28:22 +0200

    add special blocks from .json

--- app/games/game2/index.tsx
+++ app/games/game2/index.tsx
@@ -1,25 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
+import { Alert } from 'react-native';
 
 import { StyleSheet, View, Text, TouchableOpacity, TextInput, ScrollView , Image, Keyboard, Platform, Button } from 'react-native';
 const mapobject = require("@/assets/ASDmap/map.json");
 const SQUARE_SIZE = 50; // Rozmiar kwadratu
 const GRID_COLUMNS = 12; // Liczba kolumn
 const GRID_ROWS = 8; // Liczba wierszy
-        
+let collectedBeer = 0;
+let maxBeer = 0;
 
 export default function Game2() { 
   const [scriptBlocks, setScriptBlocks] = useState<any[]>([]);
   const [nextId, setNextId] = useState(0);
   const [playerPosition, setPlayerPosition] = useState({ row: 0, col: 0 });
+  const [startPlayerPosition, setStartPlayer] = useState({ row: 0, col: 0 });
   const [endPosition, setEndPosition] = useState({ row: 3, col: 3 });
   const [beer, setBeer] = useState<any[]>([]);
   const [puddle, setPuddle] = useState<any[]>([]);
   const [wall, setWall] = useState<any[]>([]);
-
+  collectedBeer = 0;
+  let startingPlayerPosition = playerPosition;
   useEffect(() =>{
     loadMapData();
   },[]);
-
+  
     const addBlockToScript = (type) => {
         let indentLevel = 0;
         if (scriptBlocks.length > 0) {
@@ -96,33 +100,61 @@ export default function Game2() {
       newCol++;
     }
 
+    const isWall = wall.some(item => item.row === newRow && item.col === newCol);
+    const isPuddle = puddle.some(item => item.row === newRow && item.col === newCol);
+    const isBeer = beer.some(item => item.row === newRow && item.col === newCol);
+    const isEnd = newRow === endPosition.row && newCol === endPosition.col;
+
+    if (isWall || isPuddle) {
+        // Gracz nie może się poruszać na pole z przeszkodą lub kałużą
+        return;
+    }
+
+    if (isBeer) {
+      // Gracz zbiera piwo
+      collectedBeer++;
+      setBeer(prevBeer => prevBeer.filter(item => item.row !== newRow || item.col !== newCol)); // Usuwamy zebrane piwo
+    }
+
     playerPosition.row = newRow;
     playerPosition.col = newCol;
     setPlayerPosition({ row: newRow, col: newCol });
+
+    if (isEnd) {
+      if (collectedBeer === maxBeer) {
+        console.log("Przeszedłeś");
+        Alert.alert('Gratulacje!', 'Przeszedłeś poziom!');
+      } else {
+        console.log("musisz zebrać piwa");
+        Alert.alert('Informacja', 'Musisz zebrać wszystkie piwa przed zakończeniem poziomu.');
+      }
+    }
+
   };
 
 
   const loadMapData = async () => {
     try {
       if (mapobject !== null) {
-        console.log("ej");
         setPlayerPosition(mapobject.playerPosition);
+        setStartPlayer(mapobject.playerPosition);
         setEndPosition(mapobject.endPosition);
         setBeer(mapobject.beer);
         setPuddle(mapobject.puddle);
         setWall(mapobject.wall);
+        maxBeer=beer.length;
       }
     } catch (error) {
       console.error('Błąd wczytywania danych z pliku map.json:', error);
     }
   };
   
-  const renderSquares = () => {
+  const renderSquares = (usePlayerPosition = true) => {
     const squares = [];
+    const playerPos = usePlayerPosition ? playerPosition : {row: 10, col:10};
     for (let row = 0; row < GRID_ROWS; row++) {
       for (let col = 0; col < GRID_COLUMNS; col++) {
-        const isGreen = (row + col) % 2 === 0;
-        const isPlayerPosition = row === playerPosition.row && col === playerPosition.col;
+        const isPlayerPosition = row === playerPos.row && col === playerPos.col;
         const isEndPosition = row === endPosition.row && col === endPosition.col;
         const isBeerPosition = beer.some(item => item.row === row && item.col === col);
         const isPuddlePosition = puddle.some(item => item.row === row && item.col === col);
@@ -150,9 +182,22 @@ export default function Game2() {
     return squares;
   };
     const runSimulation = async () => {
-        for (const block of  scriptBlocks) {
+        loadMapData();
+        collectedBeer = 0;
+        playerPosition.col = startPlayerPosition.col;
+        playerPosition.row = startPlayerPosition.row;
+        setPlayerPosition(startPlayerPosition)
+        await new Promise (resolve => setTimeout(resolve, 1000));
+        playerPosition.col = startPlayerPosition.col;
+        playerPosition.row = startPlayerPosition.row;
+        setPlayerPosition(startPlayerPosition)
+        renderSquares(false);
+
+        for (const block of scriptBlocks) {
             if (block.count !== undefined) {
                 for(let i = 0; i < block.count; i++) {
+                    await new Promise (resolve => setTimeout(resolve, 500));
+
                     switch (block.type) {
                         case 'Góra':
                             movePlayer('Up');
@@ -169,8 +214,8 @@ export default function Game2() {
                         default:
                             break;
                     }
-                    renderSquares();
-                    await new Promise (resolve => setTimeout(resolve, 1000));
+                    renderSquares(true);
+                    await new Promise (resolve => setTimeout(resolve, 500));
                 }
 
             } else {
@@ -185,7 +230,7 @@ export default function Game2() {
                 {renderScriptBlocks()}
             </ScrollView>
       <View style={styles.simulationArea}>
-          <Button title="Uruchom symulację" onPress={runSimulation} />
+        <Button title="Uruchom symulację" onPress={() => { loadMapData(); runSimulation(); }} />
         <View style={styles.grid}>
           {renderSquares()}
         </View>
