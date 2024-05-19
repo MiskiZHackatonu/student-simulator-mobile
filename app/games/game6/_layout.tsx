1715eb345cdb18f7b3d930cd9893f9d91bcfd6e6
Patryk Klatka <patryk.klatka@gmail.com>
2024-05-19 16:20:12 +0200

    Minor fixes

--- app/games/game6/_layout.tsx
+++ app/games/game6/_layout.tsx
@@ -1,5 +1,28 @@
 import { Stack } from "expo-router";
 
-const StackNavigator = () => <Stack />;
+const StackNavigator = () => (
+  <Stack>
+    <Stack.Screen
+      name="index"
+      options={{
+        headerShown: false,
+      }}
+    />
+    <Stack.Screen
+      name="matchmaking_lobby"
+      options={{
+        title: "Lobby",
+        headerBackTitle: "Zasady",
+      }}
+    />
+    <Stack.Screen
+      name="game_main"
+      options={{
+        title: "Tablica",
+        headerBackTitle: "Zasady",
+      }}
+    />
+  </Stack>
+);
 
 export default StackNavigator;
--- app/games/index.tsx
+++ app/games/index.tsx
@@ -1,6 +1,16 @@
 import { router } from "expo-router";
 import React, { useState, useCallback, useEffect, useContext } from "react";
-import { SafeAreaView, StyleSheet, FlatList, Dimensions, ImageBackground, Text, View, Pressable, Image} from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+  ImageBackground,
+  Text,
+  View,
+  Pressable,
+  Image,
+} from "react-native";
 import InfoBottomsheet from "@/components/InfoBottomsheet";
 import CircularMenu from "@/components/CircularMenu";
 import QRButton from "@/components/QRButton";
@@ -9,10 +19,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { AllGamesContext } from "./_layout";
 import { ThemedText } from "@/components/ThemedText";
 
-const { width, height } = Dimensions.get('window');
-const s1_image = require('./../../assets/images/1.jpg')
-const s2_image = require('./../../assets/images/2.jpg')
-const background_wiet = require('./../../assets/images/background_wiet.jpg')
+const { width, height } = Dimensions.get("window");
+const s1_image = require("./../../assets/images/1.jpg");
+const s2_image = require("./../../assets/images/2.jpg");
+const background_wiet = require("./../../assets/images/background_wiet.jpg");
 
 const s1_itemParams = [
   {
@@ -31,10 +41,21 @@ const s1_itemParams = [
   },
 ];
 const s2_itemParams = [
-  {rad: width / 6, ang: 50, pos_rad: 100, label: "UNIX", backgroundColor: '#F6A200'}, 
-  {rad: width / 8, ang: 110, pos_rad: 100, label: "IO", backgroundColor: '#13447E'},
-]
-
+  {
+    rad: width / 6,
+    ang: 50,
+    pos_rad: 100,
+    label: "UNIX",
+    backgroundColor: "#F6A200",
+  },
+  {
+    rad: width / 8,
+    ang: 110,
+    pos_rad: 100,
+    label: "IO",
+    backgroundColor: "#13447E",
+  },
+];
 
 const useless_items = [
   {
@@ -60,7 +81,7 @@ const useless_items = [
   },
   {
     rad: width / 12,
-    ang: 5.10,
+    ang: 5.1,
     pos_rad: 230,
     label: "NON_CLICKABLE",
     backgroundColor: "#13447E",
@@ -78,7 +99,7 @@ const useless_items = [
     pos_rad: 150,
     label: "NON_CLICKABLE",
     backgroundColor: "#84ACCE",
-  }
+  },
 ];
 
 const useless_items_s2 = [
@@ -105,7 +126,7 @@ const useless_items_s2 = [
   },
   {
     rad: width / 12,
-    ang: 5.10,
+    ang: 5.1,
     pos_rad: 230,
     label: "NON_CLICKABLE",
     backgroundColor: "#13447E",
@@ -123,9 +144,8 @@ const useless_items_s2 = [
     pos_rad: 150,
     label: "NON_CLICKABLE",
     backgroundColor: "#84ACCE",
-  }
+  },
 ];
-
 
 const data = [
   { image: s1_image, params: [...useless_items, ...s1_itemParams] },
@@ -137,26 +157,30 @@ type ParamList = {
   };
 };
 
-const Screen = ({gameInfo, setGameInfo, itemParams, image}) => {
-  const {completed} = useContext(AllGamesContext)
+const Screen = ({ gameInfo, setGameInfo, itemParams, image }) => {
+  const { completed } = useContext(AllGamesContext);
 
   return (
     <SafeAreaView style={styles.item}>
-    {/* <ImageBackground
+      {/* <ImageBackground
     source={image}
     style={styles.backgroundImage}
     resizeMode="cover"
       > */}
-    <CircularMenu setGameInfo={setGameInfo} itemParams={itemParams} completed={completed}/>
-    <InfoBottomsheet
-      key={gameInfo}
-      currentGameInfo={gameInfo}
-      setCurrentGameInfo={setGameInfo}
-    />
-    {/* </ImageBackground> */}
-  </SafeAreaView>
-  )
-}
+      <CircularMenu
+        setGameInfo={setGameInfo}
+        itemParams={itemParams}
+        completed={completed}
+      />
+      <InfoBottomsheet
+        key={gameInfo}
+        currentGameInfo={gameInfo}
+        setCurrentGameInfo={setGameInfo}
+      />
+      {/* </ImageBackground> */}
+    </SafeAreaView>
+  );
+};
 
 const App = () => {
   const [gameInfo, setGameInfo] = useState("None");
@@ -165,7 +189,7 @@ const App = () => {
   const navigation = useNavigation();
   const [backgroundOffset, setBackgroundOffset] = useState(0);
 
-  const {setNick} = useContext(AllGamesContext)
+  const { setNick } = useContext(AllGamesContext);
   setNick(nick);
 
   // const handleGameClick = (gameName: string) => {
@@ -177,16 +201,14 @@ const App = () => {
   const logOut = useCallback(async () => {
     await AsyncStorage.removeItem("nick");
     router.replace({
-      pathname: "/games/game6",
+      pathname: "/onboarding",
     });
   }, []);
 
   useEffect(() => {
     console.log(`got nick ${nick}`);
     navigation.setOptions({
-      headerTitle: () => (
-        <ThemedText>Welcome, {nick}!</ThemedText>
-      ),
+      headerTitle: () => <ThemedText>Welcome, {nick}!</ThemedText>,
       headerRight: () => (
         <Pressable onPress={logOut}>
           <ThemedText>Logout</ThemedText>
@@ -197,45 +219,54 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.item}>
-    <View>
-      <Image style={{ 
-        height: height, 
-        width: width * 3, 
-        position: 'absolute', 
-        top:-50, 
-        left:backgroundOffset - width / 2 - 250}} 
-        source={background_wiet} />
-    </View>
-    <FlatList
-      data={data}
-      onScroll={(event) => {
-        const scrolling = -event.nativeEvent.contentOffset.x;
-        setBackgroundOffset(scrolling)
-      }}
-      renderItem={({ item }) => <Screen 
-        gameInfo = {gameInfo} 
-        setGameInfo={setGameInfo} 
-        itemParams={item.params} 
-        image={item.image}/>}
-      horizontal
-      pagingEnabled
-      showsHorizontalScrollIndicator={true}
-      snapToAlignment="center"
-      decelerationRate="fast"
-    />
-    <QRButton style={{
+      <View>
+        <Image
+          style={{
+            height: height,
+            width: width * 3,
             position: "absolute",
-            bottom: 40,
-            left: 20,
-            width: 50,
-            height: 50,
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "white",
-            borderRadius: 50,
-            zIndex: 100,
-    }} onPress={() => router.push("/games/qrCamera")}/>
-    {/* </ImageBackground> */}
+            top: -50,
+            left: backgroundOffset - width / 2 - 250,
+          }}
+          source={background_wiet}
+        />
+      </View>
+      <FlatList
+        data={data}
+        onScroll={(event) => {
+          const scrolling = -event.nativeEvent.contentOffset.x;
+          setBackgroundOffset(scrolling);
+        }}
+        renderItem={({ item }) => (
+          <Screen
+            gameInfo={gameInfo}
+            setGameInfo={setGameInfo}
+            itemParams={item.params}
+            image={item.image}
+          />
+        )}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={true}
+        snapToAlignment="center"
+        decelerationRate="fast"
+      />
+      <QRButton
+        style={{
+          position: "absolute",
+          bottom: 40,
+          left: 20,
+          width: 50,
+          height: 50,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "white",
+          borderRadius: 50,
+          zIndex: 100,
+        }}
+        onPress={() => router.push("/games/qrCamera")}
+      />
+      {/* </ImageBackground> */}
     </SafeAreaView>
   );
 };
@@ -243,28 +274,28 @@ const App = () => {
 const styles = StyleSheet.create({
   item: {
     width,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
   },
   title: {
     fontSize: 32,
   },
   backgroundImage: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover'
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
   text: {
-    color: 'white',
+    color: "white",
     fontSize: 42,
     lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    backgroundColor: '#000000c0',
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
   },
 });
 
