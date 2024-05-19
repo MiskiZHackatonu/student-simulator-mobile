1e7d217a48074c12861b461735768394e8f65949
Bartłomiej Tempka <115587104+bartektempka@users.noreply.github.com>
2024-05-19 12:24:46 +0200

    game6 server

--- app/games/game6/index.tsx
+++ app/games/game6/index.tsx
@@ -6,8 +6,9 @@ export default function Game2() {
   return (
     <View>
       <Text>game1</Text>
-      <Button title= "Gracz1" onPress={() => {router.push({ pathname: "/games/game6/game_main", params: { player_id: 1 } });}}/>
-        <Button title= "Gracz2" onPress={() => {router.push({ pathname: "/games/game6/game_main", params: { player_id: 2 } });}}/>
+      <Button title= "lobby" onPress={() => {router.push({ pathname: "/games/game6/matchmaking_lobby"});}}/>
+      <Button title= "Gracz1" onPress={() => {router.push({ pathname: "/games/game6/game_main", params: { player_id: 1 ,lobby_id: "aaa"} });}}/>
+        <Button title= "Gracz2" onPress={() => {router.push({ pathname: "/games/game6/game_main", params: { player_id: 2,lobby_id: "aaa"} });}}/>
     </View>
   );
 }
