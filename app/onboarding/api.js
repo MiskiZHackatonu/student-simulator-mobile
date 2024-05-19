a8e6d66471dad5059258af3bde6c00121967a4d0
Thyen-Radek <rabuhrabuhrabuh@poczta.fm>
2024-05-19 10:35:17 +0200

    fetching for game6

--- app/onboarding/api.js
+++ app/onboarding/api.js
@@ -2,6 +2,8 @@ import { Alert } from 'react-native';
 // hard code urls
 const registerLoginUrl = 'http://localhost:3000/api/registerOrLogin'; 
 const matchmakingUrl = 'http://localhost:3000/games/6/matchmaking';
+const setScoreUrl = 'http://localhost:3000/games/setScore';
+const games6SolvedUrl = 'http://localhost:3000/games/6/solved';
 // const registerLoginUrl = 'http://172.20.10.2:3000/api/registerOrLogin'; 
 
 async function fetchMessage(url, message){
@@ -26,7 +28,6 @@ async function fetchMessage(url, message){
 export async function registerOrLogin(name) {
   var responseBody = await fetchMessage(registerLoginUrl, {name: name});
   // test matchmaking 
-  matchmaking(1, 1);
   if (responseBody.error) {
     Alert.alert('Connection Error', responseBody.message);
     console.log(responseBody);
@@ -44,5 +45,27 @@ export async function matchmaking(lobbyId, userId) {
   } else {
     Alert.alert('Success', 'Matchmaking successful');
     console.log('Matchmaking successful');
+  }
+}
+
+export async function setScore (userId, gameId, score) {
+  var responseBody = await fetchMessage(setScoreUrl, {userId: userId, gameId: gameId});
+  if (responseBody.error) {
+    Alert.alert('Connection Error', responseBody.message);
+    console.log(responseBody);
+  } else {
+    Alert.alert('Success', 'Score updated successfully');
+    console.log('Score updated successfully');
+  }
+}
+
+export async function games6Solved (userId, gameId, solved) {
+  var responseBody = await fetchMessage(games6SolvedUrl, {userId: userId, gameId: gameId});
+  if (responseBody.error) {
+    Alert.alert('Connection Error', responseBody.message);
+    console.log(responseBody);
+  } else {
+    Alert.alert('Success', 'Solved updated successfully');
+    console.log('Solved updated successfully');
   }
 }
