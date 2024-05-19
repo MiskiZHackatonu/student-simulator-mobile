008b21c2b4cfe9695b1fa800557ddc71a5b86f4c
Patryk Klatka <patryk.klatka@gmail.com>
2024-05-19 12:58:19 +0200

    Fix game1 scroll

--- app/games/game1/tutorial.tsx
+++ app/games/game1/tutorial.tsx
@@ -1,4 +1,11 @@
-import { View, TextInput, SafeAreaView, Pressable, Alert } from "react-native";
+import {
+  View,
+  TextInput,
+  SafeAreaView,
+  Pressable,
+  Alert,
+  ScrollView,
+} from "react-native";
 import React, { useState } from "react";
 import { ThemedText } from "@/components/ThemedText";
 import { router } from "expo-router";
@@ -9,86 +16,88 @@ export default function TutorialPage() {
 
   return (
     <SafeAreaView>
-      <View>
-        <ThemedText
+      <ScrollView automaticallyAdjustKeyboardInsets={true}>
+        <View>
+          <ThemedText
+            style={{
+              marginTop: 30,
+              textAlign: "center",
+            }}
+            type="title"
+          >
+            UNIX - wstęp do obsługi terminala
+          </ThemedText>
+        </View>
+        <View
           style={{
-            marginTop: 30,
-            textAlign: "center",
-          }}
-          type="title"
-        >
-          UNIX - wstęp do obsługi terminala
-        </ThemedText>
-      </View>
-      <View
-        style={{
-          marginTop: 40,
-          marginRight: "auto",
-          marginLeft: "auto",
-        }}
-      >
-        <ThemedText>
-          W grze będziesz poruszał się po terminalu komendami Unixowymi.{"\n"}
-          {"\n"}
-          Przydadtne komendy:{"\n"}
-          {"\t"}• ls - wyświetla zawartość bieżącego katalogu {"\n"}
-          {"\t"}• cd [nazwa katalogu] - zmienia katalog{"\n"}
-          {"\t"}• cat [nazwa pliku] - wyświetla zawartość pliku{"\n"}
-        </ThemedText>
-      </View>
-      <View>
-        <ThemedText
-          style={{ marginRight: "auto", marginLeft: "auto", marginTop: 40 }}
-        >
-          Podaj 4 słowo w Części I w zadaniu 1a
-        </ThemedText>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: "gray",
-            borderWidth: 1,
-            marginTop: 10,
+            marginTop: 40,
             marginRight: "auto",
             marginLeft: "auto",
-            minWidth: "50%",
-            padding: 8,
           }}
-          placeholder="Odpowiedź"
-          value={answer}
-          onChangeText={(text) => setAnswer(text)}
-        />
-        <View>
-          <Pressable
-            onPress={() => {
-              if (AnswerValidation(answer)) {
-                setResult(1);
-                Alert.alert("Poprawna odpowiedź", "Gratulacje", [
-                  {
-                    text: "OK",
-                    onPress: () => {
-                      router.replace("/games");
-                    },
-                  },
-                ]);
-              } else {
-                setResult(2);
-                Alert.alert("Błędna odpowiedź", "Spróbuj ponownie");
-              }
-            }}
-          >
-            <ThemedText
-              style={{
-                marginTop: "40%",
-                fontSize: 22,
-                textAlign: "center",
-              }}
-              type="link"
-            >
-              Sprawdź odpowiedź
-            </ThemedText>
-          </Pressable>
+        >
+          <ThemedText>
+            W grze będziesz poruszał się po terminalu komendami Unixowymi.{"\n"}
+            {"\n"}
+            Przydadtne komendy:{"\n"}
+            {"\t"}• ls - wyświetla zawartość bieżącego katalogu {"\n"}
+            {"\t"}• cd [nazwa katalogu] - zmienia katalog{"\n"}
+            {"\t"}• cat [nazwa pliku] - wyświetla zawartość pliku{"\n"}
+          </ThemedText>
         </View>
-      </View>
+        <View>
+          <ThemedText
+            style={{ marginRight: "auto", marginLeft: "auto", marginTop: 40 }}
+          >
+            Podaj 4 słowo w Części I w zadaniu 1a
+          </ThemedText>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 1,
+              marginTop: 10,
+              marginRight: "auto",
+              marginLeft: "auto",
+              minWidth: "50%",
+              padding: 8,
+            }}
+            placeholder="Odpowiedź"
+            value={answer}
+            onChangeText={(text) => setAnswer(text)}
+          />
+          <View>
+            <Pressable
+              onPress={() => {
+                if (AnswerValidation(answer)) {
+                  setResult(1);
+                  Alert.alert("Poprawna odpowiedź", "Gratulacje", [
+                    {
+                      text: "OK",
+                      onPress: () => {
+                        router.replace("/games");
+                      },
+                    },
+                  ]);
+                } else {
+                  setResult(2);
+                  Alert.alert("Błędna odpowiedź", "Spróbuj ponownie");
+                }
+              }}
+            >
+              <ThemedText
+                style={{
+                  marginTop: "40%",
+                  fontSize: 22,
+                  textAlign: "center",
+                }}
+                type="link"
+              >
+                Sprawdź odpowiedź
+              </ThemedText>
+            </Pressable>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
